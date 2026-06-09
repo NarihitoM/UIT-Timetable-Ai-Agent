667062f0b7182d5ee238e9b47d9ecd950a862977
Narihito <heinboss234@gmail.com>
2026-06-09 21:11:23 +0630

    .

--- src/Agent/telegram.model.ts
+++ src/Agent/telegram.model.ts
@@ -1,15 +1,13 @@
-import { ChatGroq } from "@langchain/groq";
+import { ChatMistralAI } from "@langchain/mistralai";
 import { ChatOpenAI } from "@langchain/openai";
 
 const mainmodel = new ChatOpenAI({
     model: "gpt-4.1-mini-2025-04-14",
-    streaming: true,
     apiKey: process.env.APIKEY,
 });
 
-const submodel = new ChatGroq({
-    model : "meta-llama/llama-4-scout-17b-16e-instruct",
-    streaming : true,
+const submodel = new ChatMistralAI({
+    model : "mistral-large-2512",
     apiKey : process.env.SUBAPIKEY,
 })
 
