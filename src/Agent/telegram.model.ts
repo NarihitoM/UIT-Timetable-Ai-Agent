b3f3cdc2f8eed870926110d431692ae8fee5b26a
Narihito <heinboss234@gmail.com>
2026-06-09 17:48:03 +0630

    feat: reasnoning effort

--- src/Agent/telegram.model.ts
+++ src/Agent/telegram.model.ts
@@ -3,7 +3,8 @@ import { ChatGroq } from "@langchain/groq";
 const model = new ChatGroq({
     model: "openai/gpt-oss-120b",
     streaming : true,
-    apiKey: process.env.APIKEY
+    apiKey: process.env.APIKEY,
+    reasoningEffort : "medium"
 });
 
 export default model;
